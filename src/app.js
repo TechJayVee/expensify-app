40046f75566b94c7def1d80646e3cc6c7d3b46e5
Jayvee Ramos <teacherjayvee@gmail.com>
2022-04-19 18:32:32 +0800

    Ramove Dummy Expense Data

--- src/app.js
+++ src/app.js
@@ -11,18 +11,18 @@ import './styles/style.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount:4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt:1000 }));
-store.dispatch(addExpense({ description: 'Rent bill', amount: 109500 }));
+// store.dispatch(addExpense({ description: 'Water bill', amount:4500 }));
+// store.dispatch(addExpense({ description: 'Gas bill', createdAt:1000 }));
+// store.dispatch(addExpense({ description: 'Rent bill', amount: 109500 }));
 // store.dispatch(setTextFilter('bill'));
 
 // setTimeout(() => {
 //   store.dispatch(setTextFilter('rent'));
 // }, 3000)
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
